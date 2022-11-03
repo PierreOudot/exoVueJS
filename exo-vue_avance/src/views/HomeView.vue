f126ac4cc6f4f4eeedb9782951045adf9212bf79
pierre <114479562+PierreOudot@users.noreply.github.com>
2022-11-03 17:01:44 +0100

    adding clickable element

--- exo-vue_avance/src/views/HomeView.vue
+++ exo-vue_avance/src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="shopItems">
-    <div class="" v-for="item in response" :key="item.index">
-      
-      {{item.image}}
+    <div class="grid grid-cols-4 gap-4" >
+      <div class="" v-for="item in response" :key="item.index">
+        <div class="h-20">
+          <h1 class="text-center">{{item.title}}</h1>
+        </div>
+        <div>
+          <img class="object-scale-down h-48 w-96" :src="item.image" :alt=item.title>
+        </div>
+        <div class="h-15">
+          <p class="text-center">{{item.price}} €  isCLicked:{{item.isClicked}}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,7 +30,8 @@ export default {
 
   data(){
     return{
-      response:[]
+      response:[],
+      cart: []
     }
   },
 
@@ -30,16 +40,26 @@ export default {
     async getShopItems(){
       try{
         let response = await axios.get("https://fakestoreapi.com/products");
-        console.log(response.data);
-        return this.response = response.data;
+        //console.log(response.data);
+       this.response = response.data;
+       this.addButtonAttribute();
+       return this.response
       }catch (error){
         console.log(error);
       }
+    },
+
+    addButtonAttribute(){
+      this.response.forEach(
+        item =>item.isClicked = false);
+      this.response.forEach(
+        item => console.log(item.isClicked));
     }
   },
 
   mounted(){
     this.getShopItems();
   }
+
 }
 </script>
